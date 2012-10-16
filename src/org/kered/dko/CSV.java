485433da1e4de57d56ea632bda32a56140dff60f
Derek Anderson <public@kered.org>
2012-10-16 18:11:22 -0500

    added another read method

--- src/org/kered/dko/CSV.java
+++ src/org/kered/dko/CSV.java
@@ -94,6 +94,31 @@ public class CSV {
 
 	/**
 	 * Creates an iterable of objects of type cls from a CSV file.
+	 * Note that this iterable will always try to read from the reader.  (no caching)
+	 * @param cls
+	 * @param f
+	 * @return
+	 */
+	public static <T extends Table> Iterable<T> read(final Class<T> cls, final Reader r) {
+		try {
+			final Constructor<T> constructor = (Constructor<T>) cls.getDeclaredConstructor(
+					new Field[0].getClass(), new Object[0].getClass(), Integer.TYPE, Integer.TYPE);
+			constructor.setAccessible(true);
+			return new Iterable<T>() {
+				@Override
+				public Iterator<T> iterator() {
+					return createIterator(cls, constructor, r, false);
+				}
+			};
+		} catch (final SecurityException e) {
+			throw new RuntimeException(e);
+		} catch (final NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Creates an iterable of objects of type cls from a CSV file.
 	 * @param cls
 	 * @param f
 	 * @return
@@ -104,120 +129,10 @@ public class CSV {
 					new Field[0].getClass(), new Object[0].getClass(), Integer.TYPE, Integer.TYPE);
 			constructor.setAccessible(true);
 			return new Iterable<T>() {
-				@SuppressWarnings("resource")
 				@Override
 				public Iterator<T> iterator() {
 					try {
-						final BufferedReader r = new BufferedReader(new FileReader(f));
-						return new Iterator<T>() {
-
-							private T next = null;
-							private boolean first = true;
-							private Field<?>[] fields = null;
-							private Constructor<?>[] fieldConstructors = null;
-
-							@Override
-							public boolean hasNext() {
-								if (next != null) return true;
-								try {
-									if (first) {
-										first  = false;
-										final String line = r.readLine();
-										if (line != null) {
-											final String[] headers = line.split(",");
-											fields = new Field<?>[headers.length];
-											fieldConstructors  = new Constructor<?>[fields.length];
-											final List<Field<?>> clsFields = cls.newInstance().FIELDS();
-											for (int i=0; i<fields.length; ++i) {
-												for (int j=0; j<clsFields.size(); ++j) {
-													if (clsFields.get(j).NAME.equals(headers[i])) {
-														fields[i] = clsFields.get(j);
-														break;
-													}
-												}
-												fieldConstructors[i] = fields[i].TYPE
-														.getDeclaredConstructor(String.class);
-												if (!fieldConstructors[i].isAccessible()) {
-													fieldConstructors[i].setAccessible(true);
-												}
-											}
-
-										}
-									}
-									final Object[] oa = new Object[fields.length];
-									int pos = 0;
-									final String line = r.readLine();
-									if (line != null) {
-							            StringBuilder sb = new StringBuilder();
-							            boolean quoted = false;
-										for (int i = 0; i < line.length(); ++i) {
-							                final char c = line.charAt(i);
-							                sb.append(c);
-							                if (c == '"') {
-							                    quoted = !quoted;
-							                }
-							                if ((!quoted && c == ',') || i == line.length()-1) {
-							                    final String s = sb.toString()
-							                    		.replaceAll(",$", "")
-							                    		.replaceAll("^\"|\"$", "")
-							                    		.replace("\"\"", "\"")
-							                    		.trim();
-							                    if (pos<oa.length) {
-								                    Object o = null;
-								                    if (s.length()>0) {
-								                    	o = fieldConstructors[pos].newInstance(s);
-								                    }
-							                    	oa[pos++] = o;
-							                    }
-							                    sb = new StringBuilder();
-							                }
-							            }
-										next = constructor.newInstance(fields, oa, 0, fields.length);
-									}
-
-								} catch (final IOException e) {
-									try {
-										r.close();
-									} catch (final IOException e1) {
-										e1.printStackTrace();
-									}
-									throw new RuntimeException(e);
-								} catch (final InstantiationException e) {
-									throw new RuntimeException(e);
-								} catch (final IllegalAccessException e) {
-									throw new RuntimeException(e);
-								} catch (final IllegalArgumentException e) {
-									throw new RuntimeException(e);
-								} catch (final InvocationTargetException e) {
-									throw new RuntimeException(e);
-								} catch (final SecurityException e) {
-									throw new RuntimeException(e);
-								} catch (final NoSuchMethodException e) {
-									throw new RuntimeException(e);
-								}
-								if (next != null) return true;
-								try {
-									r.close();
-								} catch (final IOException e) {
-									throw new RuntimeException(e);
-								}
-								return false;
-							}
-
-							@Override
-							public T next() {
-								if (next == null) hasNext();
-								final T t = next;
-								next = null;
-								return t;
-							}
-
-							@Override
-							public void remove() {
-								throw new UnsupportedOperationException();
-							}
-
-						};
+						return createIterator(cls, constructor, new FileReader(f), true);
 					} catch (final FileNotFoundException e) {
 						throw new RuntimeException(e);
 					}
@@ -228,6 +143,121 @@ public class CSV {
 		} catch (final NoSuchMethodException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private static <T extends Table> Iterator<T> createIterator(
+			final Class<T> cls, final Constructor<T> constructor,
+			final Reader _r, final boolean closeOnFinish) {
+		final BufferedReader r = new BufferedReader(_r);
+		return new Iterator<T>() {
+
+			private T next = null;
+			private boolean first = true;
+			private Field<?>[] fields = null;
+			private Constructor<?>[] fieldConstructors = null;
+
+			@Override
+			public boolean hasNext() {
+				if (next != null) return true;
+				try {
+					if (first) {
+						first  = false;
+						final String line = r.readLine();
+						if (line != null) {
+							final String[] headers = line.split(",");
+							fields = new Field<?>[headers.length];
+							fieldConstructors  = new Constructor<?>[fields.length];
+							final List<Field<?>> clsFields = cls.newInstance().FIELDS();
+							for (int i=0; i<fields.length; ++i) {
+								for (int j=0; j<clsFields.size(); ++j) {
+									if (clsFields.get(j).NAME.equals(headers[i])) {
+										fields[i] = clsFields.get(j);
+										break;
+									}
+								}
+								fieldConstructors[i] = fields[i].TYPE
+										.getDeclaredConstructor(String.class);
+								if (!fieldConstructors[i].isAccessible()) {
+									fieldConstructors[i].setAccessible(true);
+								}
+							}
+
+						}
+					}
+					final Object[] oa = new Object[fields.length];
+					int pos = 0;
+					final String line = r.readLine();
+					if (line != null) {
+			            StringBuilder sb = new StringBuilder();
+			            boolean quoted = false;
+						for (int i = 0; i < line.length(); ++i) {
+			                final char c = line.charAt(i);
+			                sb.append(c);
+			                if (c == '"') {
+			                    quoted = !quoted;
+			                }
+			                if ((!quoted && c == ',') || i == line.length()-1) {
+			                    final String s = sb.toString()
+			                    		.replaceAll(",$", "")
+			                    		.replaceAll("^\"|\"$", "")
+			                    		.replace("\"\"", "\"")
+			                    		.trim();
+			                    if (pos<oa.length) {
+				                    Object o = null;
+				                    if (s.length()>0) {
+				                    	o = fieldConstructors[pos].newInstance(s);
+				                    }
+			                    	oa[pos++] = o;
+			                    }
+			                    sb = new StringBuilder();
+			                }
+			            }
+						next = constructor.newInstance(fields, oa, 0, fields.length);
+					}
+
+				} catch (final IOException e) {
+					try {
+						if (closeOnFinish) r.close();
+					} catch (final IOException e1) {
+						e1.printStackTrace();
+					}
+					throw new RuntimeException(e);
+				} catch (final InstantiationException e) {
+					throw new RuntimeException(e);
+				} catch (final IllegalAccessException e) {
+					throw new RuntimeException(e);
+				} catch (final IllegalArgumentException e) {
+					throw new RuntimeException(e);
+				} catch (final InvocationTargetException e) {
+					throw new RuntimeException(e);
+				} catch (final SecurityException e) {
+					throw new RuntimeException(e);
+				} catch (final NoSuchMethodException e) {
+					throw new RuntimeException(e);
+				}
+				if (next != null) return true;
+				try {
+					if (closeOnFinish) r.close();
+				} catch (final IOException e) {
+					throw new RuntimeException(e);
+				}
+				return false;
+			}
+
+			@Override
+			public T next() {
+				if (next == null) hasNext();
+				final T t = next;
+				next = null;
+				return t;
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+
+		};
 	}
 
 }

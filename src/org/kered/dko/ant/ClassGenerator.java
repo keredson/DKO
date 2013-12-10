f8fe48f899147c455232f85bba471aa6d39d252b
Derek Anderson <public@kered.org>
2013-12-10 10:55:15 -0600

    added sql type: smallint unsigned

--- src/org/kered/dko/ant/ClassGenerator.java
+++ src/org/kered/dko/ant/ClassGenerator.java
@@ -1421,6 +1421,7 @@ class ClassGenerator {
 		put("int4", Integer.class);
 		put("mediumint", Integer.class);
 		put("smallint", Integer.class);
+		put("smallint unsigned", Integer.class);
 		put("tinyint", Integer.class);
 		put("bigint", Long.class);
 		put("long", Long.class);

591bc6647d76f7d7ce99ee88f9cfd8cb480499c7
Derek Anderson <public@kered.org>
2013-12-10 22:52:10 -0600

    removed OBE examples

--- examples/bugzilla/src/Test.java
+++
@@ -1,7 +0,0 @@
-import db.bugzilla3.Bug;
-
-class Test {
-	public static void main(String[] args) throws java.sql.SQLException {
-		System.out.println("you have "+ Bug.ALL.count() +" bugs in your db");
-	}
-}
